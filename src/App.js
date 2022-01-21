e7f44c463bf75549eafd9bcb592d989055c9beed
julesgoz <julesgozlan@gmail.com>
2022-01-21 08:35:32 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { Physics } from "@react-three/cannon";
+import "./App.css";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div className="Main">
+      <h2>Hello</h2>
+      <button onClick={addCubes}>Add More Cubes</button>
+      <Canvas>
+        <OrbitControls />
+        <ambientLight intensity={0.5} />
+        <Physics></Physics>
+      </Canvas>
+    </div>
+  );
 };
 
 export default App;
